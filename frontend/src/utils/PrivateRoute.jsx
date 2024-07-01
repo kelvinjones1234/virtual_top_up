1c364c59a32d340fd9a0ed5f88e16882200a2e20
kelvinjones1234 <praisejournal2@gmail.com>
2024-07-01 17:12:55 +0100

    made user true by default

--- frontend/src/utils/PrivateRoute.jsx
+++ frontend/src/utils/PrivateRoute.jsx
@@ -3,7 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../context/AuthenticationContext";
 
 const PrivateRoute = () => {
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
+  const { user } = true;
+
   return user ? <Outlet /> : <Navigate to="/authentication/login" />;
 };
 
