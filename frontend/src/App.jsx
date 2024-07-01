6704710befe708ee53400f1f8bf9e52a5aa32719
kelvinjones1234 <praisejournal2@gmail.com>
2024-07-01 17:27:12 +0100

    made user true by default

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,9 +26,9 @@ function App() {
                 className={`bg-custom-gradient w-full z-[-2] min-w-[150px] fixed top-0 left-0 min-h-screen`}
               ></div>
               <Routes>
-                <Route element={<HomePage />} path="/" />
+                {/* <Route element={<HomePage />} path="/" /> */}
                 <Route element={<LoginPage />} path="/authentication/login" />
-                <Route element={<UserDashboardPage />} path="/user/dashboard" />
+                <Route element={<UserDashboardPage />} path="/" />
                 <Route
                   element={<RegisterationPage />}
                   path="/authentication/register"
